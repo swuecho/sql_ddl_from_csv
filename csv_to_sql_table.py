6d86147e7a2e2ce6b8809b60ce358884f4523d1a
Hao Wu <hwu@intellisurvey.com>
2016-09-20 15:58:44 -0700

    update

--- csv_to_sql_table.py
+++ csv_to_sql_table.py
@@ -3,8 +3,14 @@ import pandas
 from sqlalchemy import create_engine
 import os
 
-db_string = os.environ['DATABASE_URL']
-conn = create_engine(db_string).connect()
+#db_string = os.environ['DATABASE_URL']
+from os.path import expanduser
+
+with open(expanduser('~/.pgpass'), 'r') as f:
+    host, port, database, user, password = f.read().replace('\n','').split(':')
+    database_uri = 'postgresql://{}:{}@{}/{}'.format(user, password, host, database)
+
+conn = create_engine(database_uri).connect()
 
 df = pandas.read_csv("./group_data.csv")
 df.to_sql('demo_sales', conn, index=False)
